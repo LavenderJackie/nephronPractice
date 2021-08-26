cd42e07842c2a4cc0cb6a2123286166759a85ab1
Jackson Ellenberg <jjellenberg12@gmail.com>
2021-08-25 23:38:27 -0400

    just some stuff wuw

---
+++ src/better-site/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import {useState, useEffect, useRef} from 'react'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col bg-gray-900 items-center justify-center min-h-screen py-2">
+      <div className="bg-green-500 rounded-xl flex h-60 w-full mx-20 px-20">
+      </div>
+    </div>
+  )
+}
