dcc8f5dc28d395caa45de6156853591a99a83e66
Jackson Ellenberg <jjellenberg12@gmail.com>
2021-09-07 20:27:41 -0400

    some layout stuff to frontend before adding fields n buttons

--- src/better-site/pages/index.js
+++ src/better-site/pages/index.js
@@ -3,8 +3,11 @@ import {useState, useEffect, useRef} from 'react'
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-gray-900 items-center justify-center py-2">
-      <div className="bg-green-500 rounded-xl flex h-60 w-full mx-20 px-20">
+    <div className="flex flex-col items-center justify-center py-2">
+      <div className="h-40" />
+      <div className="flex flex-row items-center justify-center">
+        <div className="bg-green-500 rounded-xl flex h-60 w-full px-20">
+        </div>
       </div>
     </div>
   )
